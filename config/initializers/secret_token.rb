c9262eab090f9fa9430ae87abb567413dca9f420
Pratompol Jiravanitchakorn <boat.pratompol@gmail.com>
2012-07-19 23:05:17 +0700

    Create new Rails application named micropost-app-workshop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicropostAppWorkshop::Application.config.secret_token = '50f5fb7ff7ae288ca9389d100b21d44191047f41d726a0d6b47bdc1ba11fc7b98206e57d7e9b35acc3f7de6d69ab1b93e5df69b278af2514c891e5002f97a171'
